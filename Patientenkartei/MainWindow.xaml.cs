ae108f15bf9df863914112251df0a6de1f6c30e8
Dexter Brings <dexter.brings@gmail.com>
2024-11-04 20:08:11 +0100

    Programm finalisiert.

--- Patientenkartei/MainWindow.xaml.cs
+++ Patientenkartei/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,35 @@ namespace Patientenkartei
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            string content = TextBoxContent.Text;
+            string filename = TextBoxFileName.Text;
+
+            using(FileStream fs = File.Create(filename + ".txt"))
+            {
+                byte[] contentConvertedToBytes = Encoding.ASCII.GetBytes(content);
+                fs.Write(contentConvertedToBytes, 0, contentConvertedToBytes.Length);
+            }
+
+            MessageBox.Show("Datei wurde Angelegt.");
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = TextBoxFileName.Text;
+
+            using (FileStream fs = File.OpenRead(filename + ".txt"))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string content = sr.ReadToEnd();
+                    TextBoxContent.Text = content;
+                }
+
+            }
         }
     }
 }
